a17bd31b165f1055c1097426ca667bb205d5d006
Rustam-carlos <47609616+Rustam-carlos@users.noreply.github.com>
2020-02-22 00:21:03 +0600

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+    Score score;
+    void Start() 
+    {
+        //включаем скрипт в начале игры чтобы управлять им
+        score = GetComponent<Score>();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    public void EndGame()
+    {
+        gameOverMenu.SetActive(true);
+        score.SetBestScore();
+    }
+}
